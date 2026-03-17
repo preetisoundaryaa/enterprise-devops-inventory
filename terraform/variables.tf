b27ece38231f4fb63ebf434cd64f1dc67f65d714
preetisoundaryaa <preetisoundaryaa@gmail.com>
2026-03-17 14:28:31 +1100

    Add optional resource group management lock support

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,33 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "allowed_locations" {
+  description = "Azure locations allowed for resource deployments"
+  type        = list(string)
+  default     = ["East US"]
+}
+
+variable "required_tags" {
+  description = "Tags required on resources"
+  type        = list(string)
+  default     = ["project", "environment"]
+}
+
+
+variable "resource_group_lock_level" {
+  description = "Management lock level for the resource group. Valid values: CanNotDelete, ReadOnly."
+  type        = string
+  default     = "CanNotDelete"
+
+  validation {
+    condition     = contains(["CanNotDelete", "ReadOnly"], var.resource_group_lock_level)
+    error_message = "resource_group_lock_level must be either CanNotDelete or ReadOnly."
+  }
+}
+
+variable "enable_resource_group_lock" {
+  description = "Whether to apply a management lock to the resource group."
+  type        = bool
+  default     = true
+}
